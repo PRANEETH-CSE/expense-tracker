dfe147cabb898dc2bd9e450d9fcabcb73720999e
PRANEETH-CSE <praneeth.m1000@gmail.com>
2025-08-23 21:18:45 +0530

    logo

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb+srv://anyone:142007@expenses.l3ftldt.mongodb.net/")
+        .then(console.log("database"))
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -8,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 
 // API routes (example)
-app.get("/api/hello", (req, res) => {
+app.get("/api/data", (req, res) => {
   res.json({ message: "Hello from Express!" });
 });
 
